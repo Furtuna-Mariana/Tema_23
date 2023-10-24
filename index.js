556caa55855282cc3629cab2e1abe24db87d9252
Furtuna-Mariana <furtuna.mariana@gmail.com>
2023-10-24 12:34:41 +0200

    third commit

--- index.js
+++ index.js
@@ -13,6 +13,14 @@
 // const existkey = arrKey (obiect, key);
 // console.log(existkey);
 
+// ===========================================
+// const checkKey = (obj, key) => {
+//     return obj.hasOwnProperty(key)
+// }
+
+// console.log(checkKey({nume: 'Alex'}, 'nume'))
+
+
 
 
 
@@ -28,6 +36,23 @@
 // }
 
 // console.log(func(char, str))
+
+
+// ============================================
+// const countVowel = (str) => {
+//     const vowelList = ['a', 'e', 'i', 'o', 'u']
+//     const checkstr = str.toLowerCase()
+//     let count = 0
+
+// // facem un forloop
+//     for(let i = 0; i < str.length; i++) {
+//         if(vowelList.includes(str[i])) count++
+//     }
+
+//     return count
+// }
+
+// console.log(countVowel('Hello world'))
 
 
 
@@ -56,6 +81,24 @@
 // const mediiStudenti = calculMedii(noteStudenti);
 // console.log(mediiStudenti);
 
+// ============================================
+// const studenti = {
+//     alex: [5,6,7,8],
+//     victor: [6,7,8,10],
+//     vasile: [8,9,7,10]
+// }
+// const checkGrandesAverage = (obj) => {
+//     const keys = Object.keys(obj)
+
+//     return keys.reduce((acc, curr) => {
+//        const average = acc[curr] = obj[curr].reduce((a, c) => a + c)/obj[curr].length.toFixed(1)
+//         // acc[curr] = 'Media: ${average}' = trebuie ghilimele diverse
+//         acc [curr] = `Media: ${average}`
+//         return acc
+//  }, {})
+// }
+
+// console.log(checkGrandesAverage(studenti))
 
 
 
@@ -73,6 +116,13 @@
 // console.log(output);
 
 
+// ============================================
+// const stringReverse = (str) => {
+//     return str.split('').reverse().join('')
+
+// }
+
+// console.log(stringReverse('Hello World'))
 
 
 
@@ -92,25 +142,37 @@
 
 
 
+// ============================================
+// const checkPolidrom = (str) => {
+//     return str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
+// }
+
+// console.log(checkPolidrom('level'))
+
+
+
+
+
 
 // function findBestMatch(k, numere) {
 //     if (numere.length === 0) {
 //         return null;
-//     }   
-
-// let numarApropiat = numere[0];
-// let diferentaMica = Math.abs(k - numere[0]);
-
-// for (let i= 1;i < numere.length; i++) {
-//     const diferenta = Math.abs(numere[i]);
-//     const (diferenta < diferentaMica) {
-//         diferentaMica = diferenta;
-//         numarApropiat = numere[i]
 //     }
-// }
+
+//     let numarApropiat = numere[0];
+//     let diferentaMica = Math.abs(k - numere[0]);
+
+//     for (let i = 1; i < numere.length; i++) {
+//         const diferenta = Math.abs(k - numere[i]);
+//         if (diferenta < diferentaMica) {
+//             diferentaMica = diferenta;
+//             numarApropiat = numere[i];
+//         }
+//     }
 
 //     return numarApropiat;
 // }
+
 // const k = 5;
 // const numere = [2, 6, 3, 8, 9, 1];
 // const result = findBestMatch(k, numere);
@@ -118,31 +180,16 @@
 // console.log(result);
 
 
-function findBestMatch(k, numere) {
-    if (numere.length === 0) {
-        return null;
-    }
+// ============================================
+// const list = [5,6,8,9,4,12]
+// const number = 7
+// const findBestMatch = (arr, k) => {
+//     return arr.reduce((acc, cur) => {
+//         return Math.abs (acc - k) < Math.abs (cur - k) ? acc : cur
+//     })
+// }
 
-    let numarApropiat = numere[0];
-    let diferentaMica = Math.abs(k - numere[0]);
-
-    for (let i = 1; i < numere.length; i++) {
-        const diferenta = Math.abs(k - numere[i]);
-        if (diferenta < diferentaMica) {
-            diferentaMica = diferenta;
-            numarApropiat = numere[i];
-        }
-    }
-
-    return numarApropiat;
-}
-
-const k = 5;
-const numere = [2, 6, 3, 8, 9, 1];
-const result = findBestMatch(k, numere);
-
-console.log(result);
-
+// console.log(findBestMatch(list, number))
 
 
 
@@ -156,3 +203,16 @@ console.log(result);
 // const output = sortareCifre(input);
 
 // console.log(output);
+
+
+// ============================================
+const list = [1,2,3,4,5,6,7,8,9]
+
+const sortArr = (arr) => {
+    const even = arr.filter((item) => item % 2 === 0).sort((a,b) => b - a)
+    const odd = arr.filter((item) => item % 2 !== 0).sort((a,b) => a - b)
+
+    return [...odd, ...even]
+}
+
+console.log(sortArr(list))
